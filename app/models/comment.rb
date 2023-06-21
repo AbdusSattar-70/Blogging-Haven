6d6b19ddab0f076f973867001e88e7bc188dd7fe
Abdus Sattar <sasorganicagro@gmail.com>
2023-06-22 02:50:31 +0600

    Define the associationin the model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,13 @@
 class Comment < ApplicationRecord
+  belongs_to :post
+  belongs_to :author, class_name: 'User'
+
+  after_create :update_comments_counter
+  after_destroy :update_comments_counter
+
+  private
+
+  def update_comments_counter
+    post.update(comments_counter: post.comments.count)
+  end
 end

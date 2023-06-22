2c69ea66d36f7540b6ba5ca400488d4d44820f89
Abdus Sattar <sasorganicagro@gmail.com>
2023-06-23 02:00:13 +0600

    Add comment model test

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,8 @@
 class Comment < ApplicationRecord
-  belongs_to :post
-  belongs_to :author, class_name: 'User'
+  belongs_to :post, foreign_key: 'post_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
-  after_create :update_comments_counter
-  after_destroy :update_comments_counter
+  after_save :update_comments_counter
 
   private
 

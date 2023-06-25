ad1ec30852a6fd08955ff99692f631765f1252ab
Abdus Sattar <sasorganicagro@gmail.com>
2023-06-25 22:56:30 +0600

    Add suggested four routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'users#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/users', to: 'users#index', as: 'users'
+  get '/users/:id', to: 'users#show', as: 'user'
+  get '/users/:id/posts', to: 'posts#index', as: 'posts'
+  get '/users/:id/posts/:id', to: 'posts#show', as: 'post'
 end

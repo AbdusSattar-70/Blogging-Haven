9269cb8bd93088f0f9cbb178431591581a0bf88a
Abdus Sattar <sasorganicagro@gmail.com>
2023-06-26 16:14:58 +0600

    Modify config/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root 'users#index'
-
-  get '/users', to: 'users#index', as: 'users'
-  get '/users/:id', to: 'users#show', as: 'user'
-  get '/users/:id/posts', to: 'posts#index', as: 'posts'
-  get '/users/:id/posts/:id', to: 'posts#show', as: 'post'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end

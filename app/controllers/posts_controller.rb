d6719d688b4a1851e685069adf0db2ad33012983
Abdus Sattar <sasorganicagro@gmail.com>
2023-06-29 02:13:36 +0600

    Modify post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,35 @@
 class PostsController < ApplicationController
+  before_action :set_user, only: %i[index new show create]
+
   def index
-    @user = User.find(params[:user_id])
     @posts = @user.posts
   end
 
+  def new
+    @post = @user.posts.build
+  end
+
+  def create
+    @post = @user.posts.build(post_params)
+
+    if @post.save
+      redirect_to user_posts_path, notice: 'Post Created Successfully'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def show
+    @post = @user.posts.find(params[:id])
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:user_id])
-    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end

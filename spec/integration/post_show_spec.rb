29d7b042d1ef22950d51d0efbf155b95679e55d5
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-07-04 17:43:17 +0530

    Add post_show spec

---
+++ spec/integration/post_show_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'Post #Show Page', type: :feature do
+    before(:each) do
+      @user = User.create(name: 'Mondol', photo: 'https://example.jpg',
+                          bio: 'He is a software engineer from Bangladesh', posts_counter: 10)
+      @post = Post.create(author: @user, title: 'test post', text: 'This is my first post', likes_counter: 15,
+                          comments_counter: 10)
+      @comment = Comment.create(post_id: @post.id, author_id: @user.id, text: 'test comment')
+      visit user_post_path(@user, @post)
+    end
+
+    it 'I can see the post\'s title.' do
+      page.has_content?(@post.title)
+    end
+    it 'I can see who wrote the post.' do
+      page.has_content?(@post.author)
+    end
+    it 'I can see how many comments it has.' do
+      page.has_content?(@post.comments_counter)
+    end
+  end

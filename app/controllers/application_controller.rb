027a1452f29e03088814221d86743df478f829bc
Abdus Sattar <sasorganicagro@gmail.com>
2023-07-05 14:25:44 +0600

    Add logic for devise in the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
- before_action :allowed_params, if: :devise_controller?
+  before_action :allowed_params, if: :devise_controller?
 
- protected
+  protected
 
   def allowed_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :current_password])
- end
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name email password current_password])
+  end
 end

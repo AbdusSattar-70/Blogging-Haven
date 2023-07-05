867e60eaf6c446ec81faa63e00feb61223728dff
Abdus Sattar <sasorganicagro@gmail.com>
2023-07-06 00:08:09 +0600

    fix bug

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,10 +2,11 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @like = @post.likes.new(author_id: current_user.id)
+
     if @like.save
-      redirect_to user_post_likes_path(current_user, @post)
+      redirect_back(fallback_location: root_path, notice: 'Liked')
     else
-      redirect_to user_post_path(current_user, @post), alert: 'Failed to create like.'
+      redirect_back(fallback_location: root_path, alert: 'Failed to create like.')
     end
   end
 end

030458722eeef53336e5e1e31d238ceed77126a6
Abdus Sattar <sasorganicagro@gmail.com>
2023-07-06 04:26:23 +0600

    Fix posts counter bug

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -3,7 +3,7 @@ module CommentsHelper
     output = ''.html_safe
     @post.recent_comments.includes(:author).each do |comment|
       output += content_tag(:div, class: 'comment_div') do
-        concat content_tag(:p, link_to(comment.author.name, user_path(@user)))
+        concat content_tag(:p, link_to(comment.author.name, user_path(comment.author)))
         concat content_tag(:p, comment.text, class: 'comment-text')
       end
     end

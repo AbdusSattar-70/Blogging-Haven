6d7487f6145aa0292a0fb66f984f6f1ab566034d
Abdus Sattar <sasorganicagro@gmail.com>
2023-07-05 14:44:13 +0600

    Modify routes,comments_helper

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  #for showing the email in the browser
+  # for showing the email in the browser
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
@@ -73,5 +73,4 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
 
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'sign_up', to: 'devise/registrations#new'
+  end
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index new create show] do
       resources :comments, only: %i[new create]

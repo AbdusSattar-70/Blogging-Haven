e10ce45bb8e936076f06cb08dc7a1df8e053d66c
Abdus Sattar <sasorganicagro@gmail.com>
2023-07-07 04:46:23 +0600

    Make changes suggested by the reviewer

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.paginate(page: params[:page], per_page: 10)

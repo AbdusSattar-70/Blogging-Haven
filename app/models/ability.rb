8501717cf087115bb1941d1874c0bb8473da6df9
Abdus Sattar <sasorganicagro@gmail.com>
2023-07-06 17:54:30 +0600

    Fix linter errors

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,16 +1,14 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the user here. For example:
-    #
-    #   return unless user.present?
-    #   can :read, :all
-    #   return unless user.admin?
-    #   can :manage, :all
-    #
+    return unless user.present?
+
+    can :read, :all
+    return unless user.admin?
+
+    can :manage, :all
+
     # The first argument to `can` is the action you are giving the user
     # permission to do.
     # If you pass :manage it will apply to every action. Other common actions

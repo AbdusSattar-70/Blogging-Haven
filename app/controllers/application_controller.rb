b842bdb970c8ce3dec7b9483778e0a45625b0f8e
Abdus Sattar <sasorganicagro@gmail.com>
2023-07-07 03:31:09 +0600

    Fix rubocop errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :allowed_params, if: :devise_controller?
 
   # Catch all CanCan errors and alert the user of the exception
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, alert: exception.message
   end
-
-
-  before_action :allowed_params, if: :devise_controller?
 
   protected
 

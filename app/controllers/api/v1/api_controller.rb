ba51b43c23a0ff2e9bd3b0fc787cb1030f9719ac
Rishi Mishra <114818980+Rishi-Mishra0704@users.noreply.github.com>
2023-07-07 18:14:21 +0530

    Update api_controller.rb

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,2 +1,12 @@
-class Api::V1::ApiController < ApplicationController
+class Api::V1::PostsController < ApplicationController
+   def index
+    user = User.find(params[:user_id])
+    posts = user.posts
+    if posts
+      render json: { status: 'Success', message: 'Loaded Posts', data: posts }, status: :ok
+    else
+      render json: { status: 'Not Found', message: 'Posts not found', data: posts.errors },
+             status: :unprocessable_entity
+    end
+  end
 end

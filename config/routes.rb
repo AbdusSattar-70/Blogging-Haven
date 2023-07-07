6c4acb3eb3ffeecbc1ca72c2139edcf00aad14ba
Abdus Sattar <sasorganicagro@gmail.com>
2023-07-07 20:30:59 +0600

    Fix authorization errors

--- config/routes.rb
+++ config/routes.rb
@@ -14,3 +14,16 @@ Rails.application.routes.draw do
       resources :comments, only: %i[new create destroy]
       resources :likes, only: [:create]
     end
+  end
+
+  # for api end point
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [] do
+        resources :posts, only: [:index] do
+          resources :comments, only: %i[index create]
+        end
+      end
+    end
+  end
+end

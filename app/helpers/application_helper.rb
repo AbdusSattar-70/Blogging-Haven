452782eb2e2db302ab518ea1e5fae4a3a11358a3
Abdus Sattar <sasorganicagro@gmail.com>
2023-07-08 23:13:24 +0600

    Fix linter error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,16 @@
 module ApplicationHelper
   def navigation
-    content_tag(:header, class: "navigation") do
+    content_tag(:header, class: 'navigation') do
       content_tag(:nav) do
         link_to('Home', root_path) +
-        link_to('Logout', destroy_user_session_path, method: :delete) +
-        link_to('Login', new_user_session_path)
+          link_to('Logout', destroy_user_session_path, method: :delete) +
+          link_to('Login', new_user_session_path)
       end
     end +
-    content_tag(:div, class: "notice alert") do
-      content_tag(:p, notice) do
-      content_tag(:p, alert)
+      content_tag(:div, class: 'notice alert') do
+        content_tag(:p, notice) do
+          content_tag(:p, alert)
+        end
       end
-    end
   end
 end
